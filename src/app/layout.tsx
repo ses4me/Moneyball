327840092e99784ef1417b5d386b0c254083a226
Aarush Jain <aarushjain10@gmail.com>
2026-02-15 06:20:00 +0530

    Fixed layout and metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,36 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+variable: "--font-geist-sans",
+subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+variable: "--font-geist-mono",
+subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "MoneyBall",
-  description: "A project by Aarush",
+title: "MoneyBall",
+description: "A project by Aarush",
 };
 
 export default function RootLayout({
-  children,
+children,
 }: Readonly<{
-  children: React.ReactNode;
+children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+return (
+<html lang="en">
+<body className={${geistSans.variable} ${geistMono.variable} antialiased}>
+{children}
+<Analytics />
+<SpeedInsights />
+</body>
+</html>
+);
 }
-<SpeedInsights/>
-<Analytics/>
